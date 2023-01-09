d83b01226da3db563d28b425bd9043a220727f97
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-01-09 23:23:57 +0900

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { CssBaseline } from "@mui/material";
+import { ApolloProvider } from "@apollo/client";
 import { Contributions } from "./components/Contributions";
 import { TopAppBar } from "./components/TopAppBar";
-import { CssBaseline } from "@mui/material";
 import { RootBody } from "./components/RootBody";
+import { client } from "./logic/github";
 
 function App() {
   return (
     <>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <CssBaseline />
-        <TopAppBar />
-        <RootBody>
-          <Contributions />
-        </RootBody>
-      </LocalizationProvider>
+      <ApolloProvider client={client}>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <CssBaseline />
+          <TopAppBar />
+          <RootBody>
+            <Contributions />
+          </RootBody>
+        </LocalizationProvider>
+      </ApolloProvider>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { client } from "./logic/github";
-import { ApolloProvider } from "@apollo/client";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 );
 
