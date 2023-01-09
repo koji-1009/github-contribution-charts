9c1e1eab5e7bf193b85340361a0a186252cb2b8d
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-01-09 14:49:24 +0900

    Update codegen

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   schema: "./schema/schema.json",
-  documents: ["src/**/*.tsx"],
+  documents: ["src/**/*.ts"],
   generates: {
     "./src/__generated__/": {
       preset: "client",
--- src/__generated__/gql.ts
+++ src/__generated__/gql.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
-import * as types from "./graphql";
-import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
+import * as types from './graphql';
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
 /**
  * Map of all GraphQL operations in the project.
@@ -13,16 +13,8 @@ import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel-plugin for production.
  */
 const documents = {
-  "\n  query Contributions($userName: String!, $from: DateTime!, $to: DateTime!) {\n    user(login: $userName) {\n      contributionsCollection(from: $from, to: $to) {\n        contributionCalendar {\n          totalContributions\n          weeks {\n            contributionDays {\n              contributionCount\n              date\n            }\n          }\n        }\n      }\n    }\n  }\n":
-    types.ContributionsDocument,
+    "\n  query Contributions($userName: String!, $from: DateTime!, $to: DateTime!) {\n    user(login: $userName) {\n      contributionsCollection(from: $from, to: $to) {\n        contributionCalendar {\n          totalContributions\n          weeks {\n            contributionDays {\n              contributionCount\n              date\n            }\n          }\n        }\n      }\n    }\n  }\n": types.ContributionsDocument,
 };
-
-/**
- * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
-export function gql(
-  source: "\n  query Contributions($userName: String!, $from: DateTime!, $to: DateTime!) {\n    user(login: $userName) {\n      contributionsCollection(from: $from, to: $to) {\n        contributionCalendar {\n          totalContributions\n          weeks {\n            contributionDays {\n              contributionCount\n              date\n            }\n          }\n        }\n      }\n    }\n  }\n"
-): typeof documents["\n  query Contributions($userName: String!, $from: DateTime!, $to: DateTime!) {\n    user(login: $userName) {\n      contributionsCollection(from: $from, to: $to) {\n        contributionCalendar {\n          totalContributions\n          weeks {\n            contributionDays {\n              contributionCount\n              date\n            }\n          }\n        }\n      }\n    }\n  }\n"];
 
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
@@ -35,12 +27,16 @@ export function gql(
  *
  * The query argument is unknown!
  * Please regenerate the types.
- **/
+**/
 export function gql(source: string): unknown;
+
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(source: "\n  query Contributions($userName: String!, $from: DateTime!, $to: DateTime!) {\n    user(login: $userName) {\n      contributionsCollection(from: $from, to: $to) {\n        contributionCalendar {\n          totalContributions\n          weeks {\n            contributionDays {\n              contributionCount\n              date\n            }\n          }\n        }\n      }\n    }\n  }\n"): (typeof documents)["\n  query Contributions($userName: String!, $from: DateTime!, $to: DateTime!) {\n    user(login: $userName) {\n      contributionsCollection(from: $from, to: $to) {\n        contributionCalendar {\n          totalContributions\n          weeks {\n            contributionDays {\n              contributionCount\n              date\n            }\n          }\n        }\n      }\n    }\n  }\n"];
 
 export function gql(source: string) {
   return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
-  TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
